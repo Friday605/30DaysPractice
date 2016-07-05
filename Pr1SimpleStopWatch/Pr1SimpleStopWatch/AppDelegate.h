e0b29ec19806ccf7c629036bbcba54608b4de8f7
Friday <zhouwu_83@163.com>
2016-07-05 16:01:15 +0800

    create

---
+++ Pr1SimpleStopWatch/Pr1SimpleStopWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pr1SimpleStopWatch
+//
+//  Created by Friday on 16/7/5.
+//  Copyright © 2016年 Friday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pr1SimpleStopWatch/Pr1SimpleStopWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pr1SimpleStopWatch
+//
+//  Created by Friday on 16/7/5.
+//  Copyright © 2016年 Friday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
